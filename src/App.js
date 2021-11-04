fa0e067353f00b33e1224f049dabb8b987183c98
Michael Orvin <morvin@MacBook-Pro-14-inch-2021.local>
2021-11-03 18:04:36 -0600

    Working through the app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import { API_KEY } from './Constants';
+import axios from "axios";
+import Image from './Components/Image';
+
+
+
 function App() {
+  const [nasaData, setNasaData] = useState()
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }, [])
   return (
     <div className="App">
       <p>
---
+++ src/Constants/index.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'JpccMminzD022D9Lkm8FegP6ZeHzoUOCJ1LLOTmC';
