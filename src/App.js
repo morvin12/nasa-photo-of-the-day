485007b488f1779f24183b424abe8e7d42b087df
Michael Orvin <morvin@MBP14inch2021.localdomain>
2021-11-04 20:57:45 -0600

    working on nasa project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import { API_KEY } from './Constants';
 import axios from "axios";
 import Image from './Components/Image';
+import styled from 'styled-components';
+import logo from './Images/nasa_logo.png';
+
 
 
 
@@ -13,7 +16,8 @@ function App() {
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     .then(res => {
-      console.log(res.data);
+      // console.log(res.data);
+      setNasaData(res.data);
     })
     .catch(err => {
       console.error(err);
@@ -21,6 +25,8 @@ function App() {
   }, [])
   return (
     <div className="App">
+      <HeaderLine>
+        <StyledLogo src={logo} alt=""/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
